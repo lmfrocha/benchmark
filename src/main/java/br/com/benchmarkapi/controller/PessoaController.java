142fbbbd71a007e8337bb2a76da1224176d3d170
Rocha Lucas <l.rocha@reply.com>
2020-03-12 10:28:14 -0300

    Implementação do Swagger, documentação de API REST

--- src/main/java/br/com/benchmarkapi/controller/PessoaController.java
+++ src/main/java/br/com/benchmarkapi/controller/PessoaController.java
@@ -13,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.benchmarkapi.model.Pessoa;
 import br.com.benchmarkapi.service.BenchmarkBuilders;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/pessoa")
+@Api(value = "Pessoa")
 public class PessoaController {
 
 	@Autowired
@@ -26,21 +29,25 @@ public class PessoaController {
 		return build.hello();
 	}
 	
+	@ApiOperation(value = "Retorna a lista de todas as pessoas da base de dados")
 	@GetMapping("/listar")
 	public ResponseEntity<?> carregar(){
 		return build.listarPessoas();
 	}
 
+	@ApiOperation(value = "Cadastra uma lista de pessoas através de um JSON")
 	@PostMapping("/lista")
 	public ResponseEntity<?> salvarListaPessoas(@RequestBody List<Pessoa> lista){
 		return build.saveListPessoa(lista);
 	}
 	
+	@ApiOperation(value = "Calcula a n-Ésima posição da sequencia de fibonacci por parametro url")
 	@GetMapping("/fibonacci/{sequencia}")
 	public ResponseEntity<?> calcular(@PathVariable Integer sequencia) {
 		return build.calcularFibboBySequencia(sequencia);
 	}
 	
+	@ApiOperation(value = "Ordena um vetor de determinado tamanho por parametro url")
 	@GetMapping("/sort/{tamanho}")
 	public ResponseEntity<?> sort(@PathVariable Integer tamanho){
 		return build.ordenarByInsertionSort(tamanho);
